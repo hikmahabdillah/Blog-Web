2bc97b3657575e71d61bc8ae8f08015bef352e09
Hikmah Aldrin Abdillah <hikmahaldrin44@gmail.com>
2024-10-20 19:40:18 +0700

    update query on auth functions

--- functions/auth.php
+++ functions/auth.php
@@ -7,11 +7,8 @@ function registerUser($email, $username, $password) {
   if (!$conn) return false;
 
   // Cek apakah email sudah terdaftar
-  $query = "SELECT * FROM Users WHERE email = :email OR username = :username";
-  $stmt = $conn->prepare($query);
-  $stmt->bindParam(':email', $email);
-  $stmt->bindParam(':username', $username);
-  $stmt->execute();
+  $query = "SELECT * FROM Users WHERE email = '$email' OR username = '$username'";
+  $stmt = $conn->query($query); 
 
   if ($stmt->fetch(PDO::FETCH_ASSOC)) {
       return false; // Email / username sudah digunakan
@@ -20,14 +17,9 @@ function registerUser($email, $username, $password) {
   // Hash password sebelum menyimpannya di database
   $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 
-  // Masukkan pengguna baru ke database
-  $query = "INSERT INTO Users (email, username, password) VALUES (:email, :username, :password)";
-  $stmt = $conn->prepare($query);
-  $stmt->bindParam(':email', $email);
-  $stmt->bindParam(':username', $username);
-  $stmt->bindParam(':password', $hashedPassword);
-  
-  if ($stmt->execute()) {
+  // Masukkan new user ke database
+  $query = "INSERT INTO Users (email, username, password) VALUES ('$email', '$username', '$hashedPassword')";
+  if ($conn->exec($query)) {
       return true; // Pendaftaran berhasil
   } else {
       return false; // Pendaftaran gagal
@@ -40,10 +32,8 @@ function loginUser($email, $password){
     if (!$conn) return false;
 
     // query untuk mendapatkan data user dari email yang diinputkan
-    $query = "SELECT * FROM Users WHERE email = :email";
-    $stmt = $conn->prepare($query);
-    $stmt->bindParam(':email', $email);
-    $stmt->execute(); //query akan di jalankan
+    $query = "SELECT * FROM Users WHERE email = '$email'";
+    $stmt = $conn->query($query); 
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
     
@@ -62,13 +52,6 @@ function loginUser($email, $password){
 function isUserLoggedIn() {
   session_start();
   return isset($_SESSION['user_id']);
-}
-// jika user belum login maka akan dikembalikan ke halaman login
-function checkLogin() {
-  if (!isUserLoggedIn()) {
-      header("Location: ./login.php");
-      exit;
-  }
 }
 
 // mengatasi logout
